6ae97b5fff738dea5905ccfff54569842329ccbe
Saheed Oladosu <oladosu777@gmail.com>
2019-01-12 18:41:14 +0100

    added the logic

--- race.js
+++ race.js
@@ -1,4 +1,15 @@
 const raceChecker = () =>{
 	let runnersAge = document.getElementById('ageTaker').value;
 	let raceNumber = Math.floor(Math.random()*1000);
+	let isRegisterEarly = document.getElementById('selector').value;
+
+	if(runnersAge >= 18 && isRegisterEarly == yes){
+
+	}else if(runnersAge>=18 && isRegisterEarly==No){
+
+	}else if(runnersAge < 18 && isRegisterEarly === yes || No){
+
+	}else{
+		
+	}
 }
