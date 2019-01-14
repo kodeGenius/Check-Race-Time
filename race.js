4cd488373f550e06b7405b8d5d0183b754417434
Saheed Oladosu <oladosu777@gmail.com>
2019-01-14 14:06:36 +0100

    Modified logic output to display on browser

--- race.js
+++ race.js
@@ -4,16 +4,16 @@ function raceChecker(){
 	let isRegisterEarly = document.getElementById('selector').value;
 
 	if(runnersAge >= 18 && isRegisterEarly=='Yes'){
-		console.log(`Since you are ${runnersAge} old and you are lucky to register early. Your race number is ${raceNumber + 1000} and you will be running at 9:30am.`)
+		document.getElementById('root').innerHTML = `Since you are ${runnersAge} old and you are lucky to register early. Your race number is ${raceNumber + 1000} and you will be running at 9:30am.`;
 
 	}else if(runnersAge>=18 && isRegisterEarly=='No'){
-		console.log(` Since you are ${runnersAge} old and you register late.Your race number is ${raceNumber} and you will be running at 11:00am.`)
+		document.getElementById('root').innerHTML = `Since you are ${runnersAge} old and you register late.Your race number is ${raceNumber} and you will be running at 11:00am.`;
 
 	}else if(runnersAge < 18 && isRegisterEarly=='Yes'|| 'No'){
-		console.log(`Since yor are ${runnersAge} old. Your race number is ${raceNumber} and you will be running at 12:00 noon.`)
+		document.getElementById('root').innerHTML = `Since yor are ${runnersAge} old. Your race number is ${raceNumber} and you will be running at 12:00 noon.`;
 
 	}else{
-		console.log(`Please see the Administrator.`)
+		document.getElementById('root').innerHTML = `Please see the Administrator.`;
 
 	}
 }
