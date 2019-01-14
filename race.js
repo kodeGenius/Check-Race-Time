603e5e1a93b3c62e1881fd8a012b9e22144b2d5f
Saheed Oladosu <oladosu777@gmail.com>
2019-01-14 11:25:47 +0100

    added statement to the 1st logic

--- race.js
+++ race.js
@@ -4,12 +4,13 @@ const raceChecker = () =>{
 	let isRegisterEarly = document.getElementById('selector').value;
 
 	if(runnersAge >= 18 && isRegisterEarly == yes){
+		console.log(`Your race number is ${raceNumber + 1000} and you will be running at 9:30am.`)
 
 	}else if(runnersAge>=18 && isRegisterEarly==No){
 
 	}else if(runnersAge < 18 && isRegisterEarly === yes || No){
 
 	}else{
-		
+
 	}
 }
