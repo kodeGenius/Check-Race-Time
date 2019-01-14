7d618d56570cbd7dca849220e282d0d21d027422
Saheed Oladosu <oladosu777@gmail.com>
2019-01-14 12:37:21 +0100

    changed js

--- race.js
+++ race.js
@@ -3,17 +3,17 @@ function raceChecker(){
 	let raceNumber = Math.floor(Math.random()*1000);
 	let isRegisterEarly = document.getElementById('selector').value;
 
-	if(runnersAge >= 18 && isRegisterEarly === Yes){
+	if(runnersAge == 18){
+		console.log(`Your race number is ${raceNumber + 1000} and you will be running at 9:30am.`)
+
+	}else if(runnersAge>18){
 		console.log(`Your race number is ${raceNumber} and you will be running at 9:30am.`)
 
-	}else if(runnersAge>=18 && isRegisterEarly===No){
-		console.log(`Your race number is ${raceNumber} and you will be running at 9:30am.`)
-
-	}else if(runnersAge < 18 && isRegisterEarly === Yes || No){
-		console.log(`Your race number is ${raceNumber} and you will be running at 9:30am.`)
+	}else if(runnersAge < 18){
+		console.log(`Your race number is ${raceNumber} and you will be running at 12:00.`)
 
 	}else{
-		console.log(`Your race number is ${raceNumber} and you will be running at 9:30am.`)
+		console.log(`Your race number is ${raceNumber} and you will be running at 12:00.`)
 
 	}
 }
