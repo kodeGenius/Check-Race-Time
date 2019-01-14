a33b62df4c59690771dda67c69f96927bf059684
Saheed Oladosu <oladosu777@gmail.com>
2019-01-14 11:31:51 +0100

    test addition

--- race.js
+++ race.js
@@ -4,13 +4,16 @@ const raceChecker = () =>{
 	let isRegisterEarly = document.getElementById('selector').value;
 
 	if(runnersAge >= 18 && isRegisterEarly == yes){
-		console.log(`Your race number is ${raceNumber + 1000} and you will be running at 9:30am.`)
+		console.log(`Your race number is ${raceNumber} and you will be running at 9:30am.`)
 
 	}else if(runnersAge>=18 && isRegisterEarly==No){
+		console.log(`Your race number is ${raceNumber} and you will be running at 9:30am.`)
 
 	}else if(runnersAge < 18 && isRegisterEarly === yes || No){
+		console.log(`Your race number is ${raceNumber} and you will be running at 9:30am.`)
 
 	}else{
+		console.log(`Your race number is ${raceNumber} and you will be running at 9:30am.`)
 
 	}
 }
